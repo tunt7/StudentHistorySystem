9074e3569b557716509bf5032706a14f88c50756
ApisitFuse <111451435+ApisitFuse@users.noreply.github.com>
2022-10-16 14:59:41 +0700

    Add files via upload

---
+++ frontend/src/models/IAc_his.ts
@@ -0,0 +1,14 @@
+import { TimerOptions } from "timers";
+
+export interface AcHisInterface {
+    AC_HIS_ID: number;
+	S_ID: number;
+	AC_ID:number;
+    ADMIN_REC:number;
+    AC_HOUR:number;
+    DATE_START:Date;
+    DATE_END:Date;
+    TIME_START:Date;
+    TIME_END:Date;
+	
+}
