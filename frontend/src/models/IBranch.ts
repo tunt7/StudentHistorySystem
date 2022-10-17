1e63b067b76909db8215a295778c21fb29aa08f0
B6309985 <111475483+B6309985@users.noreply.github.com>
2022-10-17 13:27:27 +0700

    Add files via upload

--- frontend/src/models/IBranch.ts
+++ frontend/src/models/IBranch.ts
@@ -1,8 +1,15 @@
+import { AcademyInterface } from "./IAcademy";
+import { CourseInterface } from "./ICourse";
+import { AdminInterface } from "./IAdmin";
+
 export interface BranchInterface {
-    BrID: number;
+    BrID?: number;
 	BrName: string;
 	BrContact:string;		
-	BrAcademy: string;
-	BrCourse: string;
-    AdminID: number;
+	BrAcademyID?: string;
+	BrAcademy?: AcademyInterface;
+	BrCourseID?: string;
+	BrCourse?: CourseInterface;
+    AdminID?: number;
+	Admin?: AdminInterface;
 } 
