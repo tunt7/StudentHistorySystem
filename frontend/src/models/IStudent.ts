b86759c76eced7b4ecbfec3ae1189b20b562fbe0
WanwisaTT <111443701+WanwisaTT@users.noreply.github.com>
2022-10-17 14:03:22 +0700

    Add files via upload

--- frontend/src/models/IStudent.ts
+++ frontend/src/models/IStudent.ts
@@ -1,3 +1,9 @@
+import { BTInterface } from "./IBlood_Type";
+import { ELInterface } from "./IEducation_Level";
+import { EQInterface } from "./IEducation_Qualification";
+import { AcademyInterface } from "./IAcademy"
+import { BranchInterface } from "./IBranch"
+import { TInterface } from "./ITeacher"
 export interface STDInterface {
     SID: number;
 	Sfirstname:string;
@@ -12,10 +18,16 @@ export interface STDInterface {
     Sidentity_card :string;
 
 	BTID: number;
+    Blood_Type?: BTInterface;
 	ELID: number;
+    Education_Level?:ELInterface;
 	EQID: number;
+    Education_Qualification?:EQInterface;
 
     AcademyID: number;
+    Academy?:AcademyInterface;
     BranchID: number;
+    Branch?:BranchInterface;
     TeacherID: number;
+    Teacher?:TInterface;
 }
