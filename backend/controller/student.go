3375c484b0163cf87eaea062286416cc8782b102
WanwisaTT <111443701+WanwisaTT@users.noreply.github.com>
2022-10-17 14:16:13 +0700

    Add files via upload

---
+++ backend/controller/student.go
@@ -0,0 +1,76 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tunt7/SA-Project/entity"
+)
+
+// POST /users
+func CreateSTD(c *gin.Context) {
+	var std entity.Student
+	if err := c.ShouldBindJSON(&std); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	
+	if err := entity.DB().Create(&std).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": std})
+}
+
+
+// GET /user/:id
+func GetSTD(c *gin.Context) {
+	var std entity.Student
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM students WHERE id = ?", id).Scan(&std).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": std})
+}
+
+// GET /users
+func ListSTD(c *gin.Context) {
+	// var bp []entity.Behavior_Point
+	result := []map[string]interface{}{}
+	entity.DB().Table("students").
+		Select("students.id, students.sfirstname, students.slastname, education_levels.elname").
+		Joins("left join education_levels on education_levels.id = students.el_id").
+		Find(&result)
+
+	c.JSON(http.StatusOK, gin.H{"data": result})
+
+}
+
+// DELETE /users/:id
+func DeleteSTD(c *gin.Context) {
+	id := c.Param("id")
+	if tx := entity.DB().Exec("DELETE FROM students WHERE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "student not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": id})
+}
+
+// PATCH /users
+func UpdateSTD(c *gin.Context) {
+	var std entity.Student
+	if err := c.ShouldBindJSON(&std); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if tx := entity.DB().Where("id = ?", std.ID).First(&std); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+		return
+	}
+	if err := entity.DB().Save(&std).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": std})
+}
