566f047dba905dde9a8890513bee922aaf2da93f
B6309985 <111475483+B6309985@users.noreply.github.com>
2022-10-17 19:12:44 +0700

    Add files via upload

--- frontend/src/components/BranchCreate.tsx
+++ frontend/src/components/BranchCreate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -11,11 +11,18 @@ import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert, { AlertProps } from "@mui/material/Alert";
-import { BranchInterface } from "../models/IBranch";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import Autocomplete from "@mui/material/Autocomplete";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+import { BHInterface } from "../models/IBehavior_Point";
+import { PointTypeInterface } from "../models/IPoint_Type";
+import { BehaviorTypeInterface } from "../models/IBehavior_Type";
+import { AdminInterface } from "../models/IAdmin";
+import { BranchInterface } from "../models/IBranch";
+import { CourseInterface } from "../models/ICourse";
+import { AcademyInterface } from "../models/IAcademy";
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
     props,
@@ -25,10 +32,20 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 });
 
 function BHCreate() {
-    const [date, setDate] = React.useState<Date | null>(null);
-    const [Br, setBr] = React.useState<Partial<BranchInterface>>({});
     const [success, setSuccess] = React.useState(false);
     const [error, setError] = React.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState("");
+
+    const [admin, setAdmin] = React.useState<AdminInterface[]>([]);
+    const [academy, setAcademy] = React.useState<AcademyInterface[]>([]);
+    const [course, setCourse] = React.useState<CourseInterface[]>([]);
+    const [branch, setBranch] = React.useState<BranchInterface>({});
+    
+    const apiUrl = "http://localhost:8080";
+    const requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    };
 
     const handleClose = (
         event?: React.SyntheticEvent | Event,
@@ -39,38 +56,98 @@ function BHCreate() {
         }
         setSuccess(false);
         setError(false);
-    };
+    }; 
 
     const handleInputChange = (
         event: React.ChangeEvent<{ id?: string; value: any }>
     ) => {
         const id = event.target.id as keyof typeof BHCreate;
         const { value } = event.target;
-        setBr({ ...Br, [id]: value });
+        setBranch({ ...branch, [id]: value });
     };
 
-    function submit() {
-        let data = {            
-            Name: Br.BrName ?? "",
-            Contact: Br.BrContact ?? "",
-            Academy: Br.BrAcademy ?? "",
-            AdminID: Br.AdminID ?? "",
+    const handleChange = (event: SelectChangeEvent) => {
+        const name = event.target.name as keyof typeof branch;
+        setBranch({
+            ...branch,
+            [name]: event.target.value,
+        });
+    };
+
+    const getAcademy = async () => {
+        fetch(`${apiUrl}/academies`, requestOptions)
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.data) {
+                    console.log(res.data)
+                    setAcademy(res.data);
+                }
+                else { console.log("NO DATA") }
+            });
+    };
+    
+    const getCourse = async () => {
+        fetch(`${apiUrl}/course`, requestOptions)
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.data) {
+                    console.log(res.data)
+                    setCourse(res.data);
+                }
+                else { console.log("NO DATA") }
+            });
+    };
+
+    const getAdmin = async () => {
+        fetch(`${apiUrl}/admins`, requestOptions)
+            .then((response) => response.json())
+            .then((res) => {
+                if (res.data) {
+                    console.log(res.data)
+                    setAdmin(res.data);
+                }
+                else { console.log("NO DATA") }
+            });
+    };
+
+    useEffect(() => {
+        getAcademy();
+        getCourse();
+        getAdmin();
+    }, []);
+
+    const convertType = (data: string | number | undefined) => {
+        let val = typeof data === "string" ? parseInt(data) : data;
+        return val;
+    };
+
+    async function submit() {
+        let data = {
+            
+            brname: branch.Brname ?? "",
+            contact: branch.Contact ?? "",
+            AdminID: convertType(branch.AcademyID),
+            course: convertType(branch.CourseID),
+            academies: convertType(branch.AcademyID),
         };
 
-        const apiUrl = "http://localhost:8080/behavior_points";
+        console.log(data)
+
         const requestOptions = {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data),
         };
 
-        fetch(apiUrl, requestOptions)
+        fetch(`${apiUrl}/Branches`, requestOptions)
             .then((response) => response.json())
             .then((res) => {
                 if (res.data) {
                     setSuccess(true);
+                    setErrorMessage("")
                 } else {
                     setError(true);
+                    setErrorMessage(res.error)
                 }
             });
     }
@@ -102,7 +179,7 @@ function BHCreate() {
                     <Box sx={{ paddingX: 2, paddingY: 1 }}>
                         <Typography
                             component="h2"
-                            variant="h6" 
+                            variant="h6"
                             color="primary"
                             gutterBottom
                         >
@@ -112,62 +189,30 @@ function BHCreate() {
                 </Box>
                 <Divider />
                 <Grid container spacing={3} sx={{ padding: 2 }}>
-                    
+
                     <Grid item xs={6}>
                         <FormControl fullWidth variant="outlined">
                             <p>Name</p>
                             <TextField
-                                id="LastName"
+                                id="brname"
                                 variant="outlined"
                                 type="string"
                                 size="medium"
-                                value={Br.BrName}
+                                value={branch.Brname}
                                 onChange={handleInputChange}
                             />
                         </FormControl>
                     </Grid>
-                    
+
                     <Grid item xs={6}>
                         <FormControl fullWidth variant="outlined">
                             <p>Contact</p>
                             <TextField
-                                id="Point"
+                                id="contact"
                                 variant="outlined"
                                 type="number"
                                 size="medium"
-                                InputProps={{ inputProps: { min: 1 } }} 
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                value={Br.BrContact}
-                                onChange={handleInputChange}
-                            />
-                        </FormControl>
-                    </Grid>
-
-                    <Grid item xs={6}>
-                        <FormControl fullWidth variant="outlined">
-                            <p>Academy</p>
-                            <TextField
-                                id="LastName"
-                                variant="outlined"
-                                type="string"
-                                size="medium"
-                                value={Br.BrAcademy}
-                                onChange={handleInputChange}
-                            />
-                        </FormControl>
-                    </Grid>
-
-                    <Grid item xs={6}>
-                        <FormControl fullWidth variant="outlined">
-                            <p>Course</p>
-                            <TextField
-                                id="LastName"
-                                variant="outlined"
-                                type="string"
-                                size="medium"
-                                value={Br.BrCourse}
+                                value={branch.Contact}
                                 onChange={handleInputChange}
                             />
                         </FormControl>
@@ -176,21 +221,74 @@ function BHCreate() {
                     <Grid item xs={6}>
                         <FormControl fullWidth variant="outlined">
                             <p>Admin</p>
-                            <TextField
-                                id="LastName"
-                                variant="outlined"
-                                type="string"
-                                size="medium"
-                                value={Br.AdminID}
-                                onChange={handleInputChange}
-                            />
+                            <Select
+                                native
+                                value={branch.AdminID + ""}
+                                onChange={handleChange}
+                                inputProps={{
+                                    name: "AdminID",
+                                }}
+                            >
+                                <option aria-label="None" value="">
+                                    Select Admin
+                                </option>
+                                {admin.map((item: AdminInterface) => (
+                                    <option value={item.ID} key={item.ID}>
+                                        {item.Aname}
+                                    </option>
+                                ))}
+                            </Select>
                         </FormControl>
                     </Grid>
- 
-                    
-                
 
-                    
+                    <Grid item xs={6}>
+                        <FormControl fullWidth variant="outlined">
+                            <p>Academy</p>
+                            <Select
+                                native
+                                value={branch.AcademyID + ""}
+                                onChange={handleChange}
+                                inputProps={{
+                                    name: "AcademyID",
+                                }}
+                            >
+                                <option aria-label="None" value="">
+                                    Select Academy
+                                </option>
+                                {academy.map((item: AcademyInterface) => (
+                                    <option value={item.ID} key={item.ID}>
+                                        {item.Acaname}
+                                    </option>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <FormControl fullWidth variant="outlined">
+                            <p>Course</p>
+                            <Select
+                                native
+                                value={branch.CourseID + ""}
+                                onChange={handleChange}
+                                inputProps={{
+                                    name: "CourseID",
+                                }}
+                            >
+                                <option aria-label="None" value="">
+                                    Select Course
+                                </option>
+                                {course.map((item: CourseInterface) => (
+                                    <option value={item.ID} key={item.ID}>
+                                        {item.Cname}
+                                    </option>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Grid>
+
+                   
+
                     <Grid item xs={12}>
                         <Button component={RouterLink} to="/" variant="contained">
                             Back
@@ -210,4 +308,4 @@ function BHCreate() {
     );
 }
 
-export default BHCreate; 
+export default BHCreate;
