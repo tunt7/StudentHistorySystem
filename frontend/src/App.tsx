5060bdcac78fd0d312f786b293f2811f9553393f
B6304416 <111441028+B6304416@users.noreply.github.com>
2022-10-17 13:39:38 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,16 @@ import Navbar from "./components/Navbar";
 import Behavior_Points from "./components/Behavior_Points";
 import BHCreate from "./components/BHCreate";
 import Homepage from "./components/Homepage";
-import Test from "./components/TestApi";
+import Activity from "./components/Activity";
+import ActivityCreate from "./components/ActivityCreate";
+import Ac_his from "./components/Homepage";
+import Ac_hisCreate from "./components/Homepage";
+import Branch from "./components/Homepage";
+import BranchCreate from "./components/Homepage";
+import Student from "./components/Homepage";
+import StudentCreate from "./components/Homepage";
+import Teacher from "./components/Homepage";
+import TeacherCreate from "./components/Homepage";
 export default function App() {
   return (
     <Router>
@@ -12,9 +21,18 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/Activity" element={<Activity />} />
+          <Route path="/ActivityCreate" element={<ActivityCreate />} />
+          <Route path="/Ac_his" element={<Ac_his />} />
+          <Route path="/Ac_hisCreate" element={<Ac_hisCreate />} />
           <Route path="/behavior_points" element={<Behavior_Points />} />
           <Route path="/bhcreate" element={<BHCreate />} />
-          <Route path="/testapi" element={<Test />} />
+          <Route path="/Branch" element={<Branch />} />
+          <Route path="/BranchCreate" element={<BranchCreate />} />
+          <Route path="/Student" element={<Student />} />
+          <Route path="/StudentCreate" element={<StudentCreate />} />
+          <Route path="/Teacher" element={<Teacher />} />
+          <Route path="/TeacherCreate" element={<TeacherCreate />} />
         </Routes>
       </div>
     </Router>
