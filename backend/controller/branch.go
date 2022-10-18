3213932bb27d64712e12c6124e1472b7dc9ae899
B6309985 <111475483+B6309985@users.noreply.github.com>
2022-10-18 18:59:19 +0700

    Add files via upload

--- backend/controller/branch.go
+++ backend/controller/branch.go
@@ -9,16 +9,49 @@ import (
 
 // POST /users
 func CreateBr(c *gin.Context) {
-	var br entity.Branch
-	if err := c.ShouldBindJSON(&br); err != nil {
+	var branch entity.Branch
+	var admin entity.Admin
+	var academies entity.Academy
+	var course entity.Course
+
+	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 8 จะถูก bind เข้าตัวแปร branch
+	if err := c.ShouldBindJSON(&branch); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	if err := entity.DB().Create(&br).Error; err != nil {
+
+	// 9: ค้นหา admin ด้วย id
+	if tx := entity.DB().Where("id = ?", branch.AdminID).First(&admin); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "video not found"})
+		return
+	}
+
+	// 10: ค้นหา academy ด้วย id
+	if tx := entity.DB().Where("id = ?", branch.AcademyID).First(&academies); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "resolution not found"})
+		return
+	}
+
+	// 11: ค้นหา course ด้วย id
+	if tx := entity.DB().Where("id = ?", branch.CourseID).First(&course); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "playlist not found"})
+		return
+	}
+	// 12: สร้าง branch
+	wv := entity.Branch{
+		Brname:  branch.Brname,             
+		Contact: branch.Contact,                 
+		Admin:    admin,               // โยงความสัมพันธ์กับ Entity admin
+		Academy: academies,  // โยงความสัมพันธ์กับ Entity academies
+		Course: course,     // โยงความสัมพันธ์กับ Entity course
+	}
+
+	// 13: บันทึก
+	if err := entity.DB().Create(&wv).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": br})
+	c.JSON(http.StatusCreated, gin.H{"data": wv})
 }
 
 // GET /user/:id
