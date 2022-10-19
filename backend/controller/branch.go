824be9233e83f51adcf45e5b68f741da0560c1c6
B6309985 <111475483+B6309985@users.noreply.github.com>
2022-10-19 19:36:11 +0700

    Add files via upload

--- backend/controller/branch.go
+++ backend/controller/branch.go
@@ -2,78 +2,78 @@ package controller
 
 import (
 	"net/http"
-
+  
 	"github.com/gin-gonic/gin"
 	"github.com/tunt7/SA-Project/entity" 
 )
 
 // POST /users
-func CreateBr(c *gin.Context) {
-	var branch entity.Branch
-	var admin entity.Admin
+func CreateBranches(c *gin.Context) {
+	var branches entity.Branch
+	var admins entity.Admin
 	var academies entity.Academy
-	var course entity.Course
+	var courses entity.Course
 
 	// ผลลัพธ์ที่ได้จากขั้นตอนที่ 8 จะถูก bind เข้าตัวแปร branch
-	if err := c.ShouldBindJSON(&branch); err != nil {
+	if err := c.ShouldBindJSON(&branches); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	// 9: ค้นหา admin ด้วย id
-	if tx := entity.DB().Where("id = ?", branch.AdminID).First(&admin); tx.RowsAffected == 0 {
+	if tx := entity.DB().Where("id = ?", branches.AdminID).First(&admins); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "video not found"})
 		return
 	}
 
 	// 10: ค้นหา academy ด้วย id
-	if tx := entity.DB().Where("id = ?", branch.AcademyID).First(&academies); tx.RowsAffected == 0 {
+	if tx := entity.DB().Where("id = ?", branches.AcademyID).First(&academies); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "resolution not found"})
 		return
 	}
 
 	// 11: ค้นหา course ด้วย id
-	if tx := entity.DB().Where("id = ?", branch.CourseID).First(&course); tx.RowsAffected == 0 {
+	if tx := entity.DB().Where("id = ?", branches.CourseID).First(&courses); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "playlist not found"})
 		return
 	}
 	// 12: สร้าง branch
-	wv := entity.Branch{
-		Brname:  branch.Brname,             
-		Contact: branch.Contact,                 
-		Admin:    admin,               // โยงความสัมพันธ์กับ Entity admin
+	br := entity.Branch{
+		Brname:  branches.Brname,             
+		Contact: branches.Contact,                 
+		Admin:    admins,               // โยงความสัมพันธ์กับ Entity admin
 		Academy: academies,  // โยงความสัมพันธ์กับ Entity academies
-		Course: course,     // โยงความสัมพันธ์กับ Entity course
+		Course: courses,     // โยงความสัมพันธ์กับ Entity course
 	}
 
 	// 13: บันทึก
-	if err := entity.DB().Create(&wv).Error; err != nil {
+	if err := entity.DB().Create(&br).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusCreated, gin.H{"data": wv})
+	c.JSON(http.StatusCreated, gin.H{"data": br})
 }
 
 // GET /user/:id
-func GetBr(c *gin.Context) {
-	var br entity.Branch
+func GetBranches(c *gin.Context) {
+	var branches entity.Branch
 	id := c.Param("id")
-	if err := entity.DB().Raw("SELECT * FROM branches WHERE id = ?", id).Scan(&br).Error; err != nil {
+	if err := entity.DB().Raw("SELECT * FROM branches WHERE id = ?", id).Scan(&branches).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": br})
+	c.JSON(http.StatusOK, gin.H{"data": branches})
 }
 
 // GET /users
-func ListBr(c *gin.Context) {
+func ListBranches(c *gin.Context) {
 	// var bp []entity.Behavior_Point
 	result := []map[string]interface{}{}
 	entity.DB().Table("branches").
-		Select("branches.id, branches.brname, branches.contact, admins.aname, academies.acaname, course.cname").
+		Select("branches.id, branches.brname, branches.contact, admins.aname, academies.acaname, courses.cname").
 		Joins("left join admins on admins.id = branches.admin_id").
 		Joins("left join academies on academies.id = branches.academy_id").
-		Joins("left join course on course.id = branches.courses_id").
+		Joins("left join courses on courses.id = branches.course_id").
 		Find(&result)
 
 	c.JSON(http.StatusOK, gin.H{"data": result})
@@ -81,7 +81,7 @@ func ListBr(c *gin.Context) {
 }
 
 // DELETE /users/:id
-func DeleteBr(c *gin.Context) {
+func DeleteBranches(c *gin.Context) {
 	id := c.Param("id")
 	if tx := entity.DB().Exec("DELETE FROM branches WHERE id = ?", id); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "behavior point not found"})
@@ -91,20 +91,20 @@ func DeleteBr(c *gin.Context) {
 }
 
 // PATCH /users
-func UpdateBr(c *gin.Context) {
-	var br entity.Branch
-	if err := c.ShouldBindJSON(&br); err != nil {
+func UpdateBranches(c *gin.Context) {
+	var branches entity.Branch
+	if err := c.ShouldBindJSON(&branches); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	if tx := entity.DB().Where("id = ?", br.ID).First(&br); tx.RowsAffected == 0 {
+	if tx := entity.DB().Where("id = ?", branches.ID).First(&branches); tx.RowsAffected == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
 		return
 	}
-	if err := entity.DB().Save(&br).Error; err != nil {
+	if err := entity.DB().Save(&branches).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": br})
+	c.JSON(http.StatusOK, gin.H{"data": branches})
 }
  
--- backend/controller/course.go
+++ backend/controller/course.go
@@ -1,10 +1,10 @@
 package controller
 
 import (
-	"net/http"
+	"net/http"  
 
 	"github.com/gin-gonic/gin"
-	"github.com/tunt7/SA-Project/entity"
+	"github.com/tunt7/SA-Project/entity" 
 )
 
 // POST /courses
@@ -27,7 +27,7 @@ func Getcourse(c *gin.Context) {
 	var course entity.Course
 
 	id := c.Param("id")
-	if err := entity.DB().Preload("Owner").Raw("SELECT * FROM course WHERE id = ?", id).Find(&course).Error; err != nil {
+	if err := entity.DB().Preload("Owner").Raw("SELECT * FROM courses WHERE id = ?", id).Find(&course).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -37,20 +37,20 @@ func Getcourse(c *gin.Context) {
 
 // GET /courses
 func Listcourses(c *gin.Context) {
-	var courses []entity.Course
-	if err := entity.DB().Preload("Owner").Raw("SELECT * FROM course").Find(&courses).Error; err != nil {
+	var course []entity.Course
+	if err := entity.DB().Raw("SELECT * FROM courses").Scan(&course).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"data": courses})
+	c.JSON(http.StatusOK, gin.H{"data": course})
 }
 
 // DELETE /courses/:id
 func Deletecourse(c *gin.Context) {
 	id := c.Param("id")
-	if tx := entity.DB().Exec("DELETE FROM course WHERE id = ?", id); tx.RowsAffected == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "course not found"})
+	if tx := entity.DB().Exec("DELETE FROM courses WHERE id = ?", id); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "courses not found"})
 		return
 	}
 
