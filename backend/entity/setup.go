e81c54ee3c18b01604b83ae9d9dfcf340b9fda7d
tunt7 <111451719+tunt7@users.noreply.github.com>
2022-10-22 02:54:35 +0700

    Add files via upload

--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	// "golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -19,4 +20,39 @@ func SetupDatabase() {
 
 	db = database
 
+	// password, err := bcrypt.GenerateFromPassword([]byte("123"), 14)
+	// if err != nil {
+	// 	panic("failed to generate password")
+	// }
+
+	// db.Model(&Admin{}).Create(&Admin{
+	// 	Aname:     "นางสาววันวิสา เถาโท",
+	// 	Aemail:    "w@gmail.com",
+	// 	Apassword: string(password),
+	// })
+	// db.Model(&Admin{}).Create(&Admin{
+	// 	Aname:     "นายอภิสิทธิ์ วงศ์วิศิษฐ์",
+	// 	Aemail:    "a@gmail.com",
+	// 	Apassword: string(password),
+	// })
+	// db.Model(&Admin{}).Create(&Admin{
+	// 	Aname:     "นายธีรวัฒน์ กูดกิ่ง",
+	// 	Aemail:    "t@gmail.com",
+	// 	Apassword: string(password),
+	// })
+	// db.Model(&Admin{}).Create(&Admin{
+	// 	Aname:     "นายพีรพล นนทคําจันทร์",
+	// 	Aemail:    "p@gmail.com",
+	// 	Apassword: string(password),
+	// })
+	// db.Model(&Admin{}).Create(&Admin{
+	// 	Aname:     "นายธนเดช เชิดในเมือง",
+	// 	Aemail:    "k@gmail.com",
+	// 	Apassword: string(password),
+	// })
+	// db.Model(&Admin{}).Create(&Admin{
+	// 	Aname:     "ณัฐพล จงเกษกรรม",
+	// 	Aemail:    "n@gmail.com",
+	// 	Apassword: string(password),
+	// })
 }
