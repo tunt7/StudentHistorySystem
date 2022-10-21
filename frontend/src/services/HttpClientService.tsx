164b873a88a05076dfa4a2a2c109eba34969d6e8
tunt7 <111451719+tunt7@users.noreply.github.com>
2022-10-22 03:01:18 +0700

    Create HttpClientService.tsx

---
+++ frontend/src/services/HttpClientService.tsx
@@ -0,0 +1,56 @@
+import { SigninInterface } from "../models/ISignin";
+
+const apiUrl = "http://localhost:8080";
+
+async function Login(data: SigninInterface) {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
+
+  let res = await fetch(`${apiUrl}/login`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("aid", res.data.id);
+        return res.data;
+      } else {
+        return false;
+      }
+    });
+
+  return res;
+}
+
+
+async function GetCurrentAdmin() {
+  let ca = localStorage.getItem("aid");
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+  };
+
+  let res = await fetch(
+    `${apiUrl}/admin/${ca}`, requestOptions
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.data) {
+        return res.data;
+      } else {
+        return false;
+      }
+    });
+
+    return res;
+}
+
+export {
+  Login,
+  GetCurrentAdmin,
+};
