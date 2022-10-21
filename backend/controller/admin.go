a061f5d390c4aa9f704642d3daf9fa8426fc1080
tunt7 <111451719+tunt7@users.noreply.github.com>
2022-10-22 02:48:17 +0700

    Add files via upload

--- backend/controller/admin.go
+++ backend/controller/admin.go
@@ -6,39 +6,25 @@ import (
 	"net/http"
 )
 
-// POST /Admin
-
-func CreateAdmin(c *gin.Context) {
-	var am entity.Admin
-	if err := c.ShouldBindJSON(&am); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	if err := entity.DB().Create(&am).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"data": am})
-
-}
 
 // GET /Admin/:id
 
 func GetAdmin(c *gin.Context) {
-	var am entity.Admin
+	var admin entity.Admin
 	id := c.Param("id")
-	if err := entity.DB().Raw("SELECT * FROM admins WHERE id = ?", id).Scan(&am).Error; err != nil {
+	if err := entity.DB().Raw("SELECT * FROM admins WHERE id = ?", id).Scan(&admin).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	if admin.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "id not found"})
+		return
+	}
 
-	c.JSON(http.StatusOK, gin.H{"data": am})
-
+	c.JSON(http.StatusOK, gin.H{"data": admin})
 }
 
-// GET /Activities
+// GET /Admins
 
 func ListAdmin(c *gin.Context) {
 	var am []entity.Admin
---
+++ backend/controller/authentication.go
@@ -0,0 +1,98 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tunt7/SA-Project/entity"
+	"github.com/tunt7/SA-Project/service"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// LoginPayload login body
+type LoginPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// SignUpPayload signup body
+type SignUpPayload struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// LoginResponse token response
+type LoginResponse struct {
+	Token string `json:"token"`
+	ID    uint   `json:"id"`
+}
+
+// POST /login
+func Login(c *gin.Context) {
+	var payload LoginPayload
+	var admin entity.Admin
+
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := entity.DB().Raw("SELECT * FROM admins WHERE aemail = ?", payload.Email).Scan(&admin).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(admin.Apassword), []byte(payload.Password))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "password is incerrect"})
+		return
+	}
+
+	jwtWrapper := service.JwtWrapper{
+		SecretKey:       "SvNQpBN8y3qlVrsGAYYWoJJk56LtzFHx",
+		Issuer:          "AuthService",
+		ExpirationHours: 24,
+	}
+
+	signedToken, err := jwtWrapper.GenerateToken(admin.Aemail)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "error signing token"})
+		return
+	}
+
+	tokenResponse := LoginResponse{
+		Token: signedToken,
+		ID:    admin.ID,
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": tokenResponse})
+}
+
+// POST /create
+func CreateAdmin(c *gin.Context) {
+	var payload SignUpPayload
+	var admin entity.Admin
+
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(payload.Password), 14)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "error hashing password"})
+		return
+	}
+
+	admin.Aname = payload.Name
+	admin.Aemail = payload.Email
+	admin.Apassword = string(hashPassword)
+
+	if err := entity.DB().Create(&admin).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"data": admin})
+}
