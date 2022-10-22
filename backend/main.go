13004485955f733ef6ff47a3f86f237bb713d442
kengneeha <111474082+kengneeha@users.noreply.github.com>
2022-10-22 14:21:45 +0700

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -34,6 +34,7 @@ func main() {
 			// Admin Routes
 			r.GET("/admins", controller.ListAdmin)
 			r.GET("/admin/:id", controller.GetAdmin)
+			r.POST("/admins", controller.CreateAdmin)
 			r.PATCH("/admins", controller.UpdateAdmin)
 			r.DELETE("/admins/:id", controller.DeleteAdmin)
 	
@@ -43,35 +44,79 @@ func main() {
 			r.POST("/students", controller.CreateSTD)
 			r.PATCH("/students", controller.UpdateSTD)
 			r.DELETE("/students/:id", controller.DeleteSTD)
-	
+
+			// Academy Routes
+			r.GET("/academies", controller.Listacademy)
+			r.GET("/academies/:id", controller.Getacademy)
+			r.POST("/academies", controller.Createacademy)
+			r.PATCH("/academies", controller.Updateacademy)
+			r.DELETE("/academies/:id", controller.Deleteacademy)
+
+			// Branch Routes
+			r.GET("/branches", controller.ListBranches)
+			r.GET("/branches_show", controller.ListBranchesShow)
+			r.GET("/branches/:id", controller.GetBranches)
+			r.POST("/branches", controller.CreateBranches)
+			r.PATCH("/branches", controller.UpdateBranches)
+			r.DELETE("/branches/:id", controller.DeleteBranches)
+
 			// Behavior_Point Routes
 			r.GET("/behavior_points_show", controller.ListBHShow)
 			r.GET("/behavior_point/:id", controller.GetBH)
 			r.POST("/behavior_points", controller.CreateBH)
 			r.PATCH("/behavior_points", controller.UpdateBH)
 			r.DELETE("/behavior_points/:id", controller.DeleteBH)
-	
+
 			// Behavior_Type Routes
 			r.GET("/behavior_types", controller.ListBehaviorType)
 			r.GET("/behavior_type/:id", controller.GetBehaviorType)
 			r.POST("/behavior_types", controller.CreateBehaviorType)
 			r.PATCH("/behavior_types", controller.UpdateBehaviorType)
 			r.DELETE("/behavior_types/:id", controller.DeleteBehaviorType)
-	
+
 			// Point_Type Routes
 			r.GET("/point_types", controller.ListPointType)
 			r.GET("/point_type/:id", controller.GetPointType)
 			r.POST("/point_types", controller.CreatePointType)
 			r.PATCH("/point_types", controller.UpdatePointType)
 			r.DELETE("/point_types/:id", controller.DeletePointType)
-	
+
 			// Activity Routes
 			r.GET("/Activities", controller.ListActivity)
+			r.GET("/Activities_show", controller.ListActivityShow)
 			r.GET("/Activity/:id", controller.GetActivity)
 			r.POST("/Activities", controller.CreateActivity)
 			r.PATCH("/Activities", controller.UpdateActivity)
 			r.DELETE("/Activities/:id", controller.DeleteActivity)
-		}
+
+			// Location Routes
+			r.GET("/Locations", controller.ListLocation)
+			r.GET("/Location/:id", controller.GetLocation)
+			r.POST("/Locations", controller.CreateLocation)
+			r.PATCH("/Locations", controller.UpdateLocation)
+			r.DELETE("/Locations/:id", controller.DeleteLocation)
+
+			// Teacher Routes
+			r.GET("/Teachers", controller.ListT)
+			r.GET("/Teachers_show", controller.ListTshow)
+			r.GET("/Teacher/:id", controller.GetT)
+			r.POST("/Teachers", controller.CreateT)
+			r.PATCH("/Teachers", controller.UpdateT)
+			r.DELETE("/Teachers/:id", controller.DeleteT)
+
+			// Prefix Routes
+			r.GET("/Prefixes", controller.ListPrefix)
+			r.GET("/Prefix/:id", controller.GetPrefix)
+			r.POST("/Prefixes", controller.CreatePrefix)
+			r.PATCH("/Prefixes", controller.UpdatePrefix)
+			r.DELETE("/Prefixes/:id", controller.DeletePrefix)
+			
+			// Activity History Routes
+			r.GET("/Ac_his", controller.ListACHIS)
+			r.GET("/Ac_his/:id", controller.GetACHIS)
+			r.POST("/Ac_his", controller.CreateACHIS)
+			r.PATCH("/Ac_his", controller.UpdateACHIS)
+			r.DELETE("/Ac_his/:id", controller.DeleteACHIS)		}
 	}
 
 	// Signup User Route
