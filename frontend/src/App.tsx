8bf11753acbaabe6e297a72eb78f930e37cd0bee
tunt7 <111451719+tunt7@users.noreply.github.com>
2022-10-22 16:41:14 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,111 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiDrawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
+import MenuIcon from "@mui/icons-material/Menu";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Button from "@mui/material/Button";
+
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleIcon from "@mui/icons-material/People";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
+import Homepage from "./components/Homepage";
 import Behavior_Points from "./components/Behavior_Points";
 import Behavior_PointCreate from "./components/BehaviorPointCreate";
-import Homepage from "./components/Homepage";
 import Activity from "./components/Activity";
 import ActivityCreate from "./components/ActivityCreate";
-import Ac_his from "./components/Homepage";
-import Ac_hisCreate from "./components/Homepage";
+
+import TCreate from "./components/TCreate";
+import Teacher from "./components/TeacherShow";
+import Ac_hisCreate from "./components/Ac_hisCreate";
+import Activity_His from "./components/AcHis";
+import BrCreate from "./components/BranchCreate";
 import Branch from "./components/Branch";
-import BranchCreate from "./components/BranchCreate";
-import Student from "./components/Homepage";
-import StudentCreate from "./components/Homepage";
-import TeacherShow from "./components/TeacherShow";
-import TCeate from "./components/TCreate";
-import SignIn from "./components/SignIn";
-import Button from "@mui/material/Button";
+
+const drawerWidth = 240;
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+const mdTheme = createTheme();
+
+const menu = [
+  { name: "หน้าแรก", icon: <HomeIcon />, path: "/" },
+  { name: "นักศึกษา", icon: <PeopleIcon />, path: "/Student" },
+  { name: "อาจารย์", icon: <YouTubeIcon />, path: "/TeacherShow" },
+  { name: "สาขา", icon: <YouTubeIcon />, path: "/Branch" },
+  { name: "กิจกรรม", icon: <YouTubeIcon />, path: "/Activity" },
+  { name: "กิจกรรมของนักศึกษา", icon: <YouTubeIcon />, path: "/Ac_his" },
+  { name: "คะแนนวินัย", icon: <YouTubeIcon />, path: "/Behavior_Points" },
+];
+
 
 export default function App() {
   const [token, setToken] = useState<String>("");
-
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+  
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -35,29 +121,105 @@ export default function App() {
     localStorage.clear();
     window.location.href = "/";
   };
+
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/Activity" element={<Activity />} />
-          <Route path="/ActivityCreate" element={<ActivityCreate />} />
-          <Route path="/Ac_his" element={<Ac_his />} />
-          <Route path="/Ac_hisCreate" element={<Ac_hisCreate />} />
-          <Route path="/Behavior_points" element={<Behavior_Points />} />
-          <Route path="/Behavior_pointsCreate" element={<Behavior_PointCreate />} />
-          <Route path="/Branch" element={<Branch />} />
-          <Route path="/BranchCreate" element={<BranchCreate />} />
-          <Route path="/Student" element={<Student />} />
-          <Route path="/StudentCreate" element={<StudentCreate />} />
-          <Route path="/TeacherShow" element={<TeacherShow />} />
-          <Route path="/TCreate" element={<TCeate />} />
-        </Routes>
-      </div>
-      <Button color="inherit" onClick={signout}>
-        ออกจากระบบ
-      </Button>
+      <ThemeProvider theme={mdTheme}>
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar position="absolute" open={open}>
+            <Toolbar
+              sx={{
+                pr: "24px", // keep right padding when drawer closed
+              }}
+            >
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={toggleDrawer}
+                sx={{
+                  marginRight: "36px",
+                  ...(open && { display: "none" }),
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+              >
+                System Analysis and Design 1/65
+              </Typography>
+              <Button color="inherit" onClick={signout}>
+                ออกจากระบบ
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <Drawer variant="permanent" open={open}>
+            <Toolbar
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+                px: [1],
+              }}
+            >
+              <IconButton onClick={toggleDrawer}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Toolbar>
+            <Divider />
+            <List>
+              {menu.map((item, index) => (
+                <Link
+                  to={item.path}
+                  key={item.name}
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  <ListItem button>
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.name} />
+                  </ListItem>
+                </Link>
+              ))}
+            </List>
+          </Drawer>
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === "light"
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: "100vh",
+              overflow: "auto",
+            }}
+          >
+            <Toolbar />
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/Activity" element={<Activity />} />
+              <Route path="/ActivityCreate" element={<ActivityCreate />} />
+              <Route path="/Ac_his" element={<Activity_His />} />
+              <Route path="/Ac_hisCreate" element={<Ac_hisCreate />} />
+              <Route path="/Behavior_points" element={<Behavior_Points />} />
+              <Route path="/Behavior_pointsCreate" element={<Behavior_PointCreate />} />
+              <Route path="/Branch" element={<Branch />} />
+              <Route path="/BranchCreate" element={<BrCreate />} />
+              <Route path="/TeacherShow" element={<Teacher />} />
+              <Route path="/TCreate" element={<TCreate />} />
+              </Routes>
+            </Container>
+          </Box>
+        </Box>
+      </ThemeProvider>
     </Router>
   );
+
 }
